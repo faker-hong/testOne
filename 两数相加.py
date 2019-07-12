b2e71a7bf75a3d6138c93f99984cee28567a437d
hongcheng97 <42666723+hongcheng97@users.noreply.github.com>
2019-07-12 14:15:50 +0800

    两数相加

---
+++ 两数相加.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                if nums[i] == target - nums[j]:
+                    return i,j
+                    break
+                else:
+                    continue
