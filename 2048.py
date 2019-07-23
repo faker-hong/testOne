4261b9086f3c8d0dda2b212431f43abbf5636953
hongcheng97 <42666723+hongcheng97@users.noreply.github.com>
2019-07-23 19:20:56 +0800

    update

--- 2048.py
+++ 2048.py
@@ -1,100 +1,210 @@
-import pygame
-from enum import Enum,unique
-from math import sqrt
-from random import randint
-@unique
-class Color(Enum):
-    """ÑÕÉ«"""
+#-*- coding:utf-8 -*-
 
-    RED = (255, 0, 0)
-    GREEN = (0, 255, 0)
-    BLUE = (0, 0, 255)
-    BLACK = (0, 0, 0)
-    WHITE = (255, 255, 255)
-    GRAY = (242, 242, 242)
-    @staticmethod
-    def random_color():
-        r = randint(0, 255)
-        g = randint(0, 255)
-        b = randint(0, 255)
-        return (r, g, b)
-class Ball(object):
-    def __init__(self,x,y,radius,sx,sy,color=Color.RED):
-        self.x = x
-        self.y = y
-        self.radius = radius
-        self.sx = sx
-        self.sy = sy
-        self.color = color
-        self.alive = True
-    def move(self,screen):
-        self.x += self.sx
-        self.y += self.sy
-        if self.x - self.radius <= 0 or \
-                self.x + self.radius >= screen.get_width():
-            self.sx = -self.sx
-        if self.y - self.radius <= 0 or \
-                self.y + self.radius >= screen.get_height():
-            self.sy = -self.sy
+import curses
+from random import randrange, choice # generate and place new tile
+from collections import defaultdict
 
-    def eat(self, other):
-        """³ÔÆäËûÇò"""
-        if self.alive and other.alive and self != other:
-            dx, dy = self.x - other.x, self.y - other.y
-            distance = sqrt(dx ** 2 + dy ** 2)
-            if distance < self.radius + other.radius \
-                    and self.radius > other.radius:
-                other.alive = False
-                self.radius = self.radius + int(other.radius * 0.146)
+letter_codes = [ord(ch) for ch in 'WASDRQwasdrq']
+actions = ['Up', 'Left', 'Down', 'Right', 'Restart', 'Exit']
+actions_dict = dict(zip(letter_codes, actions * 2))
+
+def get_user_action(keyboard):    
+    char = "N"
+    while char not in actions_dict:    
+        char = keyboard.getch()
+    return actions_dict[char]
+
+def transpose(field):
+    return [list(row) for row in zip(*field)]
+
+def invert(field):
+    return [row[::-1] for row in field]
+
+class GameField(object):
+    def __init__(self, height=4, width=4, win=2048):
+        self.height = height
+        self.width = width
+        self.win_value = win
+        self.score = 0
+        self.highscore = 0
+        self.reset()
+
+    def reset(self):
+        if self.score > self.highscore:
+            self.highscore = self.score
+        self.score = 0
+        self.field = [[0 for i in range(self.width)] for j in range(self.height)]
+        self.spawn()
+        self.spawn()
+
+    def move(self, direction):
+        def move_row_left(row):
+            def tighten(row): # squeese non-zero elements together
+                new_row = [i for i in row if i != 0]
+                new_row += [0 for i in range(len(row) - len(new_row))]
+                return new_row
+
+            def merge(row):
+                pair = False
+                new_row = []
+                for i in range(len(row)):
+                    if pair:
+                        new_row.append(2 * row[i])
+                        self.score += 2 * row[i]
+                        pair = False
+                    else:
+                        if i + 1 < len(row) and row[i] == row[i + 1]:
+                            pair = True
+                            new_row.append(0)
+                        else:
+                            new_row.append(row[i])
+                assert len(new_row) == len(row)
+                return new_row
+            return tighten(merge(tighten(row)))
+
+        moves = {}
+        moves['Left']  = lambda field:                              \
+                [move_row_left(row) for row in field]
+        moves['Right'] = lambda field:                              \
+                invert(moves['Left'](invert(field)))
+        moves['Up']    = lambda field:                              \
+                transpose(moves['Left'](transpose(field)))
+        moves['Down']  = lambda field:                              \
+                transpose(moves['Right'](transpose(field)))
+
+        if direction in moves:
+            if self.move_is_possible(direction):
+                self.field = moves[direction](self.field)
+                self.spawn()
+                return True
+            else:
+                return False
+
+    def is_win(self):
+        return any(any(i >= self.win_value for i in row) for row in self.field)
+
+    def is_gameover(self):
+        return not any(self.move_is_possible(move) for move in actions)
 
     def draw(self, screen):
-        """ÔÚ´°¿ÚÉÏ»æÖÆÇò"""
-        pygame.draw.circle(screen, self.color,
-                           (self.x, self.y), self.radius, 0)
-def main():
-    # ¶¨ÒåÓÃÀ´×°ËùÓĞÇòµÄÈİÆ÷
-    balls = []
-    # ³õÊ¼»¯µ¼ÈëµÄpygameÖĞµÄÄ£¿é
-    pygame.init()
-    # ³õÊ¼»¯ÓÃÓÚÏÔÊ¾µÄ´°¿Ú²¢ÉèÖÃ´°¿Ú³ß´ç
-    screen = pygame.display.set_mode((800, 600))
-    # ÉèÖÃµ±Ç°´°¿ÚµÄ±êÌâ
-    pygame.display.set_caption('´óÇò³ÔĞ¡Çò')
-    running = True
-    # ¿ªÆôÒ»¸öÊÂ¼şÑ­»·´¦Àí·¢ÉúµÄÊÂ¼ş
-    while running:
-        # ´ÓÏûÏ¢¶ÓÁĞÖĞ»ñÈ¡ÊÂ¼ş²¢¶ÔÊÂ¼ş½øĞĞ´¦Àí
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-            # ´¦ÀíÊó±êÊÂ¼şµÄ´úÂë
-            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-                # »ñµÃµã»÷Êó±êµÄÎ»ÖÃ
-                x, y = event.pos
-                radius = randint(10, 100)
-                sx, sy = randint(-10, 10), randint(-10, 10)
-                color = Color.random_color()
-                # ÔÚµã»÷Êó±êµÄÎ»ÖÃ´´½¨Ò»¸öÇò(´óĞ¡¡¢ËÙ¶ÈºÍÑÕÉ«Ëæ»ú)
-                ball = Ball(x, y, radius, sx, sy, color)
-                # ½«ÇòÌí¼Óµ½ÁĞ±íÈİÆ÷ÖĞ
-                balls.append(ball)
-        screen.fill((255, 255, 255))
-        # È¡³öÈİÆ÷ÖĞµÄÇò Èç¹ûÃ»±»³Ôµô¾Í»æÖÆ ±»³ÔµôÁË¾ÍÒÆ³ı
-        for ball in balls:
-            if ball.alive:
-                ball.draw(screen)
+        help_string1 = '(W)Up (S)Down (A)Left (D)Right'
+        help_string2 = '     (R)Restart (Q)Exit'
+        gameover_string = '           GAME OVER'
+        win_string = '          YOU WIN!'
+        def cast(string):
+            screen.addstr(string + '\n')
+
+        def draw_hor_separator():
+            line = '+' + ('+------' * self.wid th + '+')[1:]
+            separator = defaultdict(lambda: line)
+            if not hasattr(draw_hor_separator, "counter"):
+                draw_hor_separator.counter = 0
+            cast(separator[draw_hor_separator.counter])
+            draw_hor_separator.counter += 1
+
+        def draw_row(row):
+            cast(''.join('|{: ^5} '.format(num) if num > 0 else '|      ' for num in row) + '|')
+
+        screen.clear()
+        cast('SCORE: ' + str(self.score))
+        if 0 != self.highscore:
+            cast('HIGHSCORE: ' + str(self.highscore))
+        for row in self.field:
+            draw_hor_separator()
+            draw_row(row)
+        draw_hor_separator()
+        if self.is_win():
+            cast(win_string)
+        else:
+            if self.is_gameover():
+                cast(gameover_string)
             else:
-                balls.remove(ball)
-        pygame.display.flip()
-        # Ã¿¸ô50ºÁÃë¾Í¸Ä±äÇòµÄÎ»ÖÃÔÙË¢ĞÂ´°¿Ú
-        pygame.time.delay(50)
-        for ball in balls:
-            ball.move(screen)
-            # ¼ì²éÇòÓĞÃ»ÓĞ³Ôµ½ÆäËûµÄÇò
-            for other in balls:
-                ball.eat(other)
+                cast(help_string1)
+        cast(help_string2)
+
+    def spawn(self):
+        new_element = 4 if randrange(100) > 89 else 2
+        (i,j) = choice([(i,j) for i in range(self.width) for j in range(self.height) if self.field[i][j] == 0])
+        self.field[i][j] = new_element
+
+    def move_is_possible(self, direction):
+        def row_is_left_movable(row): 
+            def change(i): # true if there'll be change in i-th tile
+                if row[i] == 0 and row[i + 1] != 0: # Move
+                    return True
+                if row[i] != 0 and row[i + 1] == row[i]: # Merge
+                    return True
+                return False
+            return any(change(i) for i in range(len(row) - 1))
+
+        check = {}
+        check['Left']  = lambda field:                              \
+                any(row_is_left_movable(row) for row in field)
+
+        check['Right'] = lambda field:                              \
+                 check['Left'](invert(field))
+
+        check['Up']    = lambda field:                              \
+                check['Left'](transpose(field))
+
+        check['Down']  = lambda field:                              \
+                check['Right'](transpose(field))
+
+        if direction in check:
+            return check[direction](self.field)
+        else:
+            return False
+
+def main(stdscr):
+    def init():
+        #é‡ç½®æ¸¸æˆæ£‹ç›˜
+        game_field.reset()
+        return 'Game'
+
+    def not_game(state):
+        #ç”»å‡º GameOver æˆ–è€… Win çš„ç•Œé¢
+        game_field.draw(stdscr)
+        #è¯»å–ç”¨æˆ·è¾“å…¥å¾—åˆ°actionï¼Œåˆ¤æ–­æ˜¯é‡å¯æ¸¸æˆè¿˜æ˜¯ç»“æŸæ¸¸æˆ
+        action = get_user_action(stdscr)
+        responses = defaultdict(lambda: state) #é»˜è®¤æ˜¯å½“å‰çŠ¶æ€ï¼Œæ²¡æœ‰è¡Œä¸ºå°±ä¼šä¸€ç›´åœ¨å½“å‰ç•Œé¢å¾ªç¯
+        responses['Restart'], responses['Exit'] = 'Init', 'Exit' #å¯¹åº”ä¸åŒçš„è¡Œä¸ºè½¬æ¢åˆ°ä¸åŒçš„çŠ¶æ€
+        return responses[action]
+
+    def game():
+        #ç”»å‡ºå½“å‰æ£‹ç›˜çŠ¶æ€
+        game_field.draw(stdscr)
+        #è¯»å–ç”¨æˆ·è¾“å…¥å¾—åˆ°action
+        action = get_user_action(stdscr)
+
+        if action == 'Restart':
+            return 'Init'
+        if action == 'Exit':
+            return 'Exit'
+        if game_field.move(action): # move successful
+            if game_field.is_win():
+                return 'Win'
+            if game_field.is_gameover():
+                return 'Gameover'
+        return 'Game'
 
 
-if __name__ == '__main__':
-    main()
+    state_actions = {
+            'Init': init,
+            'Win': lambda: not_game('Win'),
+            'Gameover': lambda: not_game('Gameover'),
+            'Game': game
+        }
 
+    curses.use_default_colors()
+
+    # è®¾ç½®ç»ˆç»“çŠ¶æ€æœ€å¤§æ•°å€¼ä¸º 32
+    game_field = GameField(win=32)
+
+
+    state = 'Init'
+
+    #çŠ¶æ€æœºå¼€å§‹å¾ªç¯
+    while state != 'Exit':
+        state = state_actions[state]()
+
+curses.wrapper(main)
