a77d79f1aa30a0ef7a2333b4bc878fc8912eba97
hongcheng97 <42666723+hongcheng97@users.noreply.github.com>
2019-09-03 10:24:56 +0800

    最长回文子串

--- gerrty&dynamic-programming/最长回文子串.py
+++ gerrty&dynamic-programming/最长回文子串.py
@@ -1,26 +1,40 @@
-def longestRalindrome(s):
-    l = len(s)
-    max_length = 0
-    palindromic = ''
-    if len(s) == 1:
-        return s
-    for i in range(l):
-        for j in range(i + 1, l):
-            is_palindromic = True
-            for k in range(i, int((i + j) / 2) + 1):
-                if s[k] != s[j - k + i]:
-                    is_palindromic = False
-                    break
-            if is_palindromic and (j - i + 1) > max_length:
-                max_length = j - i + 1
-                palindromic = s[i:j + 1]
-    if palindromic == '':
-        palindromic = s[0]
-    return palindromic
+class Solution(object):
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        '''超出时间限制
+        if len(s) <= 1:
+            return s
+        res = []
+        for end in range(1, len(s)+1):
+            for start in range(end):
+                if s[start:end] == s[start:end][::-1]:
+                    res.append((start, end))
+        res.sort(key=lambda x:x[1]-x[0])
+        re = res[-1]
+        return s[re[0]:re[1]]
+        '''
+        n = len(s)
+        self.res = ''
+
+        def find(i, j):
+            while i >= 0 and j < n and s[i] == s[j]:
+                i -= 1
+                j += 1
+            if len(self.res) < j-i-1:
+                # 这里的i，j已经做过加减，所以i要+1，j不变
+                self.res = s[i+1:j]
+        for i in range(n):
+            # 区别奇偶性
+            find(i, i)
+            find(i, i+1)
+        return self.res
 
 
 if __name__ == '__main__':
-    # s = "bcaab"
-    # print(s[1:4])
-    # print(longestRalindrome(s))
-    print(ord('z'))
+    s = Solution()
+    strs = 'babad'
+    re = s.longestPalindrome(strs)
+    print(re)
