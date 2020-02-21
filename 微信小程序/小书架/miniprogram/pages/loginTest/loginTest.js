bc7cbead3b49941746b7944097a8f28611643b38
hongcheng97 <42666723+hongcheng97@users.noreply.github.com>
2020-02-21 17:47:07 +0800

    小书架

---
+++ 微信小程序/小书架/miniprogram/pages/loginTest/loginTest.js
@@ -0,0 +1,82 @@
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    
+  },
+
+  formSubmit: function(e){
+    console.log(e.detail.value)
+    if(e.detail.value.zhanghao == '123' && e.detail.value.password == '123'){
+      wx.navigateTo({
+        url: '../login/login',
+      })
+    }else{
+      wx.showModal({
+        title: '错误',
+        content: '账号或密码错误',
+      })
+    }
+  },
+
+  formReset: function(){
+
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
+  }
+})
