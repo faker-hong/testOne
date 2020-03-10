a720fe23ef829d6ab925055e5a1864e51c013479
hongcheng97 <42666723+hongcheng97@users.noreply.github.com>
2020-03-10 16:16:14 +0800

    R7 optimized

--- SQL_demo练习/R7优化.sql
+++ SQL_demo练习/R7优化.sql
@@ -1,14 +1,8 @@
 1.将日期和购物方式笛卡尔积，获得所需的表结构, diff_date_type
 SELECT
-	spend_date,
-	platform
+	DISTINCT spend_date
 FROM
-	(SELECT
-		spend_date
-	FROM
-	spending
-	GROUP BY
-	spend_date) DISTINCT_Date,
+	spending,
 	(SELECT 'desktop' platform
 		UNION
 		SELECT 'mobile'
@@ -50,47 +44,64 @@ GROUP BY
 
 
 
+
 3.最终结果
 SELECT
 	table_structure.spend_date,
 	table_structure.platform,
-	IFNULL(total.total_amount,0) total_amount,
-	IFNULL(total.total_users,0) total_users
+	IFNULL(total.total_amount, 0) total_amount,
+	IFNULL(total.total_users, 0) total_users
 FROM
-	(SELECT
-		spend_date,
-		platform
-	FROM
-		(SELECT
-			spend_date
+	(
+		SELECT DISTINCT
+			(spend_date),
+			platform_type.platform
 		FROM
-		spending
-		GROUP BY
-		spend_date) DISTINCT_Date,
-		(SELECT 'desktop' platform
-			UNION
-			SELECT 'mobile'
-			UNION
-			SELECT 'both') platform_type
+			spending,
+			(
+				SELECT
+					'desktop' platform
+				UNION
+					SELECT
+						'mobile' platform
+					UNION
+						SELECT
+							'both' platform
+			) platform_type
 	) table_structure
-	LEFT JOIN
-	(SELECT
-		spend_date, 
+LEFT JOIN (
+	SELECT
+		spend_date,
 		platform,
 		SUM(amount) total_amount,
-		COUNT(*) total_users
+		COUNT(user_id) total_users
 	FROM
-		(SELECT
-		user_id, 
-		spend_date,
-		(CASE COUNT(DISTINCT platform) WHEN 1 THEN platform WHEN 2 THEN 'both' END) platform,
-		SUM(amount) amount
-	FROM
-		spending
-	GROUP BY
-		user_id, spend_date
+		(
+			SELECT
+				user_id,
+				spend_date,
+				(
+					CASE COUNT(DISTINCT platform)
+					WHEN 1 THEN
+						platform
+					WHEN 2 THEN
+						'both'
+					END
+				) platform,
+				SUM(amount) amount
+			FROM
+				spending
+			GROUP BY
+				user_id,
+				spend_date
 		) three_platform
 	GROUP BY
-		spend_date, platform
-	) total
-	ON table_structure.spend_date=total.spend_date AND table_structure.platform=total.platform
+		spend_date,	
+		platform
+) total ON table_structure.spend_date = total.spend_date
+AND table_structure.platform = total.platform
+
+
+
+
+
