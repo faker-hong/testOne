6a6751ecc592047864150789fbd6d7a38da556c9
hongcheng97 <42666723+hongcheng97@users.noreply.github.com>
2020-03-27 17:57:45 +0800

    predict price

---
+++ python人工智能深入/boston_housing/boston_housing.py
@@ -0,0 +1,100 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import ShuffleSplit, train_test_split
+from sklearn.metrics import r2_score
+
+
+data = pd.read_csv('./housing.csv')
+prices = data['MEDV']
+features = data.drop('MEDV', axis=1)
+
+
+# TODO: Minimum price of the data
+minimum_price = prices.min()
+
+# TODO: Maximum price of the data
+maximum_price = prices.max()
+
+# TODO: Mean price of the data
+mean_price = prices.mean()
+
+# TODO: Median price of the data
+median_price = prices.median()
+
+# TODO: Standard deviation of prices of the data
+std_price = prices.std()
+
+# Show the calculated statistics
+print("Statistics for Boston housing dataset:\n")
+print("Minimum price: ${}".format(minimum_price))
+print("Maximum price: ${}".format(maximum_price))
+print("Mean price: ${}".format(mean_price))
+print("Median price ${}".format(median_price))
+print("Standard deviation of prices: ${}".format(std_price))
+
+
+def performance_metric(y_true, y_predict):
+    """ Calculates and returns the performance score between
+        true and predicted values based on the metric chosen. """
+
+    # TODO: Calculate the performance score between 'y_true' and 'y_predict'
+    score = r2_score(y_true, y_predict)
+
+    # Return the score
+    return score
+
+
+# TODO: Shuffle and split the data into training and testing subsets
+X_train, X_test, y_train, y_test = train_test_split(features, prices, test_size=0.2, random_state=10)
+
+
+# TODO: Import 'make_scorer', 'DecisionTreeRegressor', and 'GridSearchCV'
+from sklearn.metrics import make_scorer
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.model_selection import GridSearchCV
+
+
+def fit_model(X, y):
+    """ Performs grid search over the 'max_depth' parameter for a
+        decision tree regressor trained on the input data [X, y]. """
+
+    # Create cross-validation sets from the training data
+    cv_sets = ShuffleSplit(n_splits=10, test_size=0.20, random_state=0)
+
+    # TODO: Create a decision tree regressor object
+    regressor = DecisionTreeRegressor()
+
+    # TODO: Create a dictionary for the parameter 'max_depth' with a range from 1 to 10
+    params = {'max_depth': [i for i in range(1, 11)]}
+
+    # TODO: Transform 'performance_metric' into a scoring function using 'make_scorer'
+    scoring_fnc = make_scorer(performance_metric)
+
+    # TODO: Create the grid search cv object --> GridSearchCV()
+    # Make sure to include the right parameters in the object:
+    # (estimator, param_grid, scoring, cv) which have values 'regressor', 'params', 'scoring_fnc', and 'cv_sets' respectively.
+    grid = GridSearchCV(regressor, params, scoring=scoring_fnc, cv=cv_sets)
+
+    # Fit the grid search object to the data to compute the optimal model
+    grid = grid.fit(X, y)
+
+    # Return the optimal model after fitting the data
+    return grid.best_estimator_
+
+
+# Fit the training data to the model using grid search
+reg = fit_model(X_train, y_train)
+
+# Produce the value for 'max_depth'
+print("Parameter 'max_depth' is {} for the optimal model.".format(reg.get_params()['max_depth']))
+
+
+
+# Produce a matrix for client data
+client_data = [[5, 17, 15], # Client 1
+               [4, 32, 22], # Client 2
+               [8, 3, 12]]  # Client 3
+
+# Show predictions
+for i, price in enumerate(reg.predict(client_data)):
+    print("Predicted selling price for Client {}'s home: ${:,.2f}".format(i+1, price))
