b881810c5de1ea0316dfe45d2b62a1adfa1cffc4
hongcheng97 <42666723+hongcheng97@users.noreply.github.com>
2020-06-08 13:30:49 +0800

    intro to tensorflow

---
+++ 人工智能/python人工智能深度学习/part_2_Convolutional Neural Networks/Intro_to_TensorFlow/TensorFlow_linear_function/quiz.py
@@ -0,0 +1,36 @@
+# Solution is available in the other "quiz_solution.py" tab
+import tensorflow as tf
+
+def get_weights(n_features, n_labels):
+    """
+    Return TensorFlow weights
+    :param n_features: Number of features
+    :param n_labels: Number of labels
+    :return: TensorFlow weights
+    """
+    # TODO: Return weights
+    weights = tf.Variable(tf.truncated_normal((n_features, n_labels)))
+    return weights
+
+
+def get_biases(n_labels):
+    """
+    Return TensorFlow bias
+    :param n_labels: Number of labels
+    :return: TensorFlow bias
+    """
+    # TODO: Return biases
+    biases = tf.Variable(n_labels)
+    return biases
+
+
+def linear(input, w, b):
+    """
+    Return linear function in TensorFlow
+    :param input: TensorFlow input
+    :param w: TensorFlow weights
+    :param b: TensorFlow biases
+    :return: TensorFlow linear function
+    """
+    # TODO: Linear Function (xW + b)
+    return tf.add(tf.matmul(input, w), b)
