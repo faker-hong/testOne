7ba581158a8060b12e53310bf032ed57f1323812
hongcheng97 <42666723+hongcheng97@users.noreply.github.com>
2020-10-26 13:36:14 +0800

    leetcode exercise

---
+++ leetcode(多线程,DP,贪心,SQL)/二刷DP与贪心LeetCode/动态规划/368. 最大整除子集/solution.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def largestDivisibleSubset(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        if not nums:
+            return []
+        nums.sort()
+        dp = [[i] for i in nums]
+
+        for i in range(len(nums)):
+            for j in range(i - 1, -1, -1):
+                if nums[i] % nums[j] == 0:
+                    if len(dp[j]) + 1 > len(dp[i]):
+                        dp[i] = dp[j] + [nums[i]]
+
+        return max(dp, key=len)
